ae06966f2344faf8e86772e1a8278af02e6ebf02
Jackie Jensen <Jackiehartvig@hotmail.com>
2023-05-17 20:35:19 +0200

    Added Privacy policy

---
+++ iosApp/iosApp/Views/EventSearchView.swift
@@ -0,0 +1,21 @@
+//
+//  EventSearchView.swift
+//  iosApp
+//
+//  Created by Jackie Jensen on 19/04/2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct EventSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EventSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        EventSearchView()
+    }
+}
